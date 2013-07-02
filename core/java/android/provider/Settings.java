8b26a24715b7efef714f5eb578c7bae63f1ee29a
dhacker29 <davidhackerdvm@gmail.com>
2013-07-01 22:03:58 -0500

    Add MVNO_ROAMING: Part of Port CM9 features to CM10
    Change-Id: I875aa080a23c214c7001458497eacbb03aa7bbf5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2705,6 +2705,12 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Whether national data roaming should be used.
+         * @hide
+         */
+        public static final String MVNO_ROAMING = "mvno_roaming";
+
+        /**
          * Whether to enable quiet hours.
          * @hide
          */
